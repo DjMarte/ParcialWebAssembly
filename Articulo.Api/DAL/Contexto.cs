335c7a6fbb4e0113bc17e9c23c3babf672c41ecb
DjMarte <alphat098@gmail.com>
2024-06-23 00:30:52 -0400

    Creé la carpeta DAL y el Contexto

---
+++ Articulo.Api/DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+
+namespace Articulo.Api.DAL
+{
+	public class Contexto : DbContext
+	{
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+		public DbSet<Articulos> Articulos { get; set; }
+    }
+}
