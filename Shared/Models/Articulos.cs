04d8e87c8c6d7cadc8d11a478083deb9a50280b8
DjMarte <alphat098@gmail.com>
2024-06-23 00:21:53 -0400

    Creé una Librería, además creé la carpeta Models con su modelo.

---
+++ Shared/Models/Articulos.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Models
+{
+	public class Articulos
+	{
+		[Key]
+		public int ArticuloId { get; set; }
+
+		[RegularExpression(@"^[a-zA-Z0-9ñÑ\s]+$")]
+		[Required(ErrorMessage = "Descripcion obligatorio")]
+		public string? Descripcion { get; set; }
+
+		[Range(0.01, 1000000, ErrorMessage = "Ingrese un valor mayor a 0.01 y menor o igual a 1000000")]
+		[Required(ErrorMessage = "Costo obligatorio")]
+		public decimal Costo { get; set; }
+
+		[Range(0, 100, ErrorMessage = "Solo se permite de 0 a 100")]
+		[Required(ErrorMessage = "Ganancia obligatoria")]
+		public decimal Ganancia { get; set; }
+
+		[Range(0.01, 1000000, ErrorMessage = "Ingrese un valor mayor a 0.01 y menor o igual a 1000000")]
+		[Required(ErrorMessage = "Precio obligatorio")]
+		public decimal Precio { get; set; }
+	}
+}
