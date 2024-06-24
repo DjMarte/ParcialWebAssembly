9abdc0a494905c111e8a936d79f13fcb50d12188
DjMarte <alphat098@gmail.com>
2024-06-24 05:19:34 -0400

    Inyecté los servicios de la Interfaz y URL.

--- ParcialWebAssembly.Client/Program.cs
+++ ParcialWebAssembly.Client/Program.cs
@@ -1,5 +1,12 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ParcialWebAssembly.Client.Services;
+using Shared.Interfaces;
+using Shared.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<IClientService<Articulos>, ArticuloService>();
 
 await builder.Build().RunAsync();
