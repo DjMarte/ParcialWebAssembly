ee8a7011d41daec883467410e8fbeee887917b31
DjMarte <alphat098@gmail.com>
2024-06-30 23:46:36 -0400

    Agregué mi LocalHost

--- ParcialWebAssembly.Client/Program.cs
+++ ParcialWebAssembly.Client/Program.cs
@@ -5,7 +5,7 @@ using Shared.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7252/") });
 
 builder.Services.AddScoped<IClientService<Articulos>, ArticuloService>();
 
