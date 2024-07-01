acce363c9ab65b442d4465f1650764b1314b8396
DjMarte <alphat098@gmail.com>
2024-06-30 23:45:47 -0400

    Creé la carpeta Interfaces y las interfaces del cliente e Api.

--- Shared/Interfaces/IApiService.cs
+++ Shared/Interfaces/IApiService.cs
@@ -1,13 +1,10 @@
-﻿using Shared.Models;
+﻿namespace Shared.Interfaces;
 
-namespace Shared.Interfaces;
-
-public interface IClientService<T>
+public interface IApiService<T>
 {
 	public Task<List<T>> GetAllAsync();
 	public Task<T> GetByIdAsync(int id);
 	public Task<T> CreateAsync(T articulo);
 	public Task<bool> UpdateAsync(T articulo);
 	public Task<bool> DeleteAsync(int id);
-	public Task<bool> ExisteDescripcionAsync(int id, string descripcion);
 }
